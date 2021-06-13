c4f9c6b705fa5f034b353d29ae962d331ed03ded
Alex Rabelo Ferreira <arf92@live.com>
2021-06-12 21:15:21 -0300

    #2 - update: codigo para formatacao checkstyle

--- src/main/java/dev/gojava/GreetingResource.java
+++ src/main/java/dev/gojava/GreetingResource.java
@@ -8,9 +8,9 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello-resteasy")
 public class GreetingResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
-    }
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello() {
+		return "Hello RESTEasy";
+	}
 }
