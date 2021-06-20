b986fa9ed204fc74e6af6fbda17b55075a867f0d
Alex Rabelo Ferreira <arf92@live.com>
2021-06-20 14:36:46 -0300

    #18 - add: services para certificado
    - service de leitura de arquivo com dados de participantes
    - service de gerar certificado
    - service de exportar certificado em pdf com metadados
    - add commands para passar params para services

---
+++ src/main/java/dev/gojava/module/certificado/service/generator/GoJavaGenerator.java
@@ -0,0 +1,179 @@
+package dev.gojava.module.certificado.service.generator;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.ColumnText;
+import com.itextpdf.text.pdf.PdfContentByte;
+import com.itextpdf.text.pdf.PdfGState;
+import com.itextpdf.text.pdf.PdfWriter;
+import dev.gojava.core.helper.DateHelper;
+import dev.gojava.core.helper.FileHelper;
+import dev.gojava.core.helper.StreamHelper;
+import dev.gojava.core.util.ParticipantUtil;
+import dev.gojava.module.certificado.command.CertificatorGeneratorCommand;
+import dev.gojava.module.certificado.model.Certificate;
+import dev.gojava.module.certificado.model.Participant;
+import dev.gojava.module.certificado.service.generator.token.TokenGenerator;
+import org.apache.commons.io.FileUtils;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@ApplicationScoped
+@Default
+public class GoJavaGenerator implements CertificateGenerator {
+
+	@Inject
+	TokenGenerator tokenGenerator;
+
+	@Override
+	public List<Certificate> generateCertificates(CertificatorGeneratorCommand command) {
+		List<Certificate> certificateList = new ArrayList<>();
+
+		List<Participant> participantList = command.getParticipantList();
+
+		for (Participant participant : participantList) {
+			Certificate certificate = new Certificate();
+			certificate.setParticipant(participant);
+
+			certificate.setFileName(createCertName(participant));
+			certificate.setFileExtension("pdf");
+			certificate.setUuid(tokenGenerator.generateToken(certificate));
+			certificate.setFileContent(buildPdfFileContent(certificate, command));
+
+			certificateList.add(certificate);
+		}
+
+		return certificateList;
+	}
+
+	private byte[] buildPdfFileContent(Certificate certificate, CertificatorGeneratorCommand command) {
+		FileOutputStream fileOutputStream = null;
+		Document document = null;
+
+		try {
+			File certificateTemp = File.createTempFile("certificateTemp_", ".pdf");
+			fileOutputStream = new FileOutputStream(certificateTemp);
+
+			document = new Document(PageSize.A4.rotate());
+			PdfWriter writer = PdfWriter.getInstance(document, fileOutputStream);
+			document.open();
+
+			ColumnText ct = new ColumnText(writer.getDirectContent());
+			ct.setSimpleColumn(75, 200, 775, 480);
+
+			Font paragraphFont = createFontToCertText();
+			Paragraph paragraph = createParagraphToCertText(certificate, paragraphFont);
+			ct.addElement(paragraph);
+			ct.go();
+
+			addBackgroundImage(writer.getDirectContentUnder(), command);
+
+			document.close();
+			certificateTemp.deleteOnExit();
+
+			return FileUtils.readFileToByteArray(certificateTemp);
+		} catch (IOException | DocumentException | URISyntaxException e) {
+			e.printStackTrace();
+		} finally {
+			if (document != null) {
+				document.close();
+			}
+			StreamHelper.closeSafeOutput(fileOutputStream);
+		}
+
+		return null;
+	}
+
+	private void addBackgroundImage(PdfContentByte background, CertificatorGeneratorCommand command) throws IOException, URISyntaxException, DocumentException {
+		String backgroundFileName = command.getBackgroundFileName();
+
+		URL backgroundUrl = getClass().getResource("/img/gojava_certificado-2-0-0.png").toURI().toURL();
+		if (FileHelper.isValidFile(backgroundFileName)) {
+			backgroundUrl = new File(backgroundFileName).toURI().toURL();
+		}
+
+		Image image = Image.getInstance(backgroundUrl);
+		image.setAbsolutePosition(0, 0);
+		image.scaleAbsolute(PageSize.A4.rotate());
+		background.saveState();
+		PdfGState pdfGState = new PdfGState();
+		pdfGState.setFillOpacity(0.8f);
+		background.setGState(pdfGState);
+		background.addImage(image);
+		background.restoreState();
+	}
+
+	private Paragraph createParagraphToCertText(Certificate certificate, Font paragraphFont) {
+		Paragraph paragraph = new Paragraph();
+		paragraph.setLeading(0f, 1.1f);
+		paragraph.setAlignment(Paragraph.ALIGN_JUSTIFIED);
+		paragraph.setFont(paragraphFont);
+		paragraph.add(generateCertificateText(certificate));
+		return paragraph;
+	}
+
+	private Font createFontToCertText() {
+		Font paragraphFont = new Font();
+		paragraphFont.setSize(24);
+		paragraphFont.setColor(BaseColor.WHITE);
+		paragraphFont.setFamily(BaseFont.TIMES_ROMAN);
+		paragraphFont.setStyle(Font.NORMAL);
+		return paragraphFont;
+	}
+
+	private String generateCertificateText(Certificate certificate) {
+		String finalText = "Certificamos que NOME_PARTICIPANTE com TIPO_IDENTIFICACAO IDENTIFICACAO_PARTICIPANTE participou do evento NOME_EVENTO durante HORAS_EVENTO horas no dia DATA_EVENTO promovido pelo Gojava - Grupo de usuários Java de Goiás. O evento foi sobre ASSUNTO_EVENTO. Valide seu certificado em gojava.dev com o token de validação: VALIDATOR_UUID";
+		Participant participant = certificate.getParticipant();
+		finalText = finalText.replaceAll("NOME_PARTICIPANTE", ParticipantUtil.participantCompleteName(participant).toUpperCase());
+		if (participant.getCpf() != null) {
+			finalText = finalText.replaceAll("TIPO_IDENTIFICACAO", "CPF");
+			finalText = finalText.replaceAll("IDENTIFICACAO_PARTICIPANTE", participant.getCpf());
+		} else {
+			finalText = finalText.replaceAll("TIPO_IDENTIFICACAO", "RG");
+			finalText = finalText.replaceAll("IDENTIFICACAO_PARTICIPANTE", participant.getRg());
+		}
+		finalText = finalText.replaceAll("NOME_EVENTO", participant.getEvent().getName());
+		finalText = finalText.replaceAll("HORAS_EVENTO", participant.getHour());
+
+		Date dateStarted = participant.getEvent().getDateStarted();
+		Date dateEnded = participant.getEvent().getDateEnded();
+		if (dateEnded != null && !dateEnded.equals(dateStarted)) {
+			String dateStartedString = DateHelper.format(dateStarted, DateFormat.SHORT);
+			String dateEndedString = DateHelper.format(dateEnded, DateFormat.SHORT);
+			finalText = finalText.replaceAll("DATA_EVENTO", String.format("%s - %s", dateStartedString, dateEndedString));
+		} else {
+			finalText = finalText.replaceAll("DATA_EVENTO", DateHelper.format(dateStarted, DateFormat.SHORT));
+		}
+
+		finalText = finalText.replaceAll("ASSUNTO_EVENTO", participant.getEvent().getTalkerTopics());
+		finalText = finalText.replaceAll("VALIDATOR_UUID", certificate.getUuid());
+
+		return finalText;
+	}
+
+	private String createCertName(Participant participant) {
+		String identy = participant.getCpf();
+		if (identy == null || identy.isEmpty()) {
+			identy = participant.getRg();
+		}
+
+		return "certificado_" + identy;
+	}
+}
