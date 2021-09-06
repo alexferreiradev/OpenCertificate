9f64217c65a9d62dac5b094701b962b421dea047
João Marcos Ribeiro Carvalho <joaomarcosribeiroc@gmail.com>
2021-09-06 00:57:20 -0300

    Adiciona testes unitários para CertificateUtil

---
+++ backend/src/test/java/dev/gojava/core/util/CertificateUtilTest.java
@@ -0,0 +1,51 @@
+package dev.gojava.core.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import dev.gojava.module.certificado.model.Certificate;
+/**
+ * Unit tests for {@link CertificateUtil}.
+ *
+ * @author João Carvalho (joaomarcosribeiroc@gmail.com)
+ */
+public final class CertificateUtilTest {
+
+    @Test
+    public void givenCertificateNull_WhenCreateFileName_ThenThrowsIllegalArgumentException(){
+            Assertions.assertThrows(IllegalArgumentException.class, new Executable(){
+            @Override
+            public void execute() throws Throwable{
+                Certificate certificate = null;
+                CertificateUtil.createFileName(certificate);
+            }
+        });
+    }
+    
+    @Test
+    public void givenCertificateNull_WhenCreateFileName_ThenMessageReturnedProperly(){
+        Certificate certificate = null;
+        String message = null;
+        try{
+            CertificateUtil.createFileName(certificate);
+        }catch(IllegalArgumentException iae){
+            message = iae.getMessage();
+        }
+        Assertions.assertEquals("Certificado não é válido, não possue nome de arquivo ou extensão",message);
+    }
+
+    @Test
+    public void givenCertificateCorrect_WhenCreateFileName_ThenCertifacateNameReturnedProperly(){
+        final String fileName = "filename";
+        final String fileExtension = "fileExtension";
+
+        Certificate certificate = new Certificate();
+        certificate.setFileName(fileName);
+        certificate.setFileExtension(fileExtension);
+
+        String resultingFileName = CertificateUtil.createFileName(certificate);
+
+        Assertions.assertEquals(resultingFileName, new StringBuilder(fileName).append(".").append(fileExtension).toString());
+    }
+}
