375fbdd3a3abaa121e73e3711bf209c7a6fb2d21
Alex Rabelo Ferreira <arf92@live.com>
2021-10-24 08:13:00 -0300

    update: eslint rules
    - add regras para dev comentadas para facilitar para o dev
    - add regras de console

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -17,12 +17,18 @@ module.exports = {
   },
   plugins: ['prettier', 'react'],
   rules: {
+    // <!-- somente em dev -->
+    // 'no-unused-expressions': 'warn', // <!-- somente em dev -->
+    // 'no-unused-vars': 'warn', // <!-- somente em dev -->
+    // 'no-console': ['warn', { allow: ['error', 'warn', 'info'] }],
+
     'prettier/prettier': [
       'warn',
       {
         endOfLine: 'auto',
       },
     ],
+    'no-console': ['error', { allow: ['error', 'warn', 'info'] }],
     'import/prefer-default-export': 'off',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
   },
