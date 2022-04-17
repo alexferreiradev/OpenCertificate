a67390daba5f833bd80a854bb77a71b61ec52406
Alex Rabelo Ferreira <arf92@live.com>
2022-04-17 16:51:34 -0300

    add: Footer component

---
+++ frontend/src/components/Footer/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+
+  min-height: 300px;
+  background-color: #444;
+  color: #fff;
+  h5 {
+    margin: 0;
+    padding: 0;
+  }
+`;
