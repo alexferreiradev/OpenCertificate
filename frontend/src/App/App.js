605ec73e24e36a4d0012cb3c68322dbf2f1e26cf
Alex Rabelo Ferreira <arf92@live.com>
2022-04-17 16:58:39 -0300

    update: App with Footer
    - add Footer component in all pages

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import { Container } from 'semantic-ui-react';
 import Routes from '../Routes/Routes';
+import Footer from '../components/Footer';
 
 function App() {
   return (
     <Container style={{ height: '100%' }}>
       <Routes />
+      <Footer />
       <ToastContainer />
     </Container>
   );
