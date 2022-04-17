d5d0f85c7190c763dc8cd7b4274e7015678f9e4a
Alex Rabelo Ferreira <arf92@live.com>
2022-04-17 16:51:09 -0300

    update: routes with gerar-certificado
    - add route to gerar certificados page

--- frontend/src/Routes/Routes.js
+++ frontend/src/Routes/Routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import React from 'react';
 import paths from './paths';
 import Home from '../pages/Home';
+import GerarCertificado from '../pages/certificado/GerarCertificado';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path={paths.home} component={Home} />
+        <Route path={paths.gerar} component={GerarCertificado} />
         <Route path="*">
           <Redirect to={paths.home} />
         </Route>
--- frontend/src/Routes/paths.js
+++ frontend/src/Routes/paths.js
@@ -1,3 +1,4 @@
 export default {
   home: '/home',
+  gerar: '/gerar-certificado',
 };
