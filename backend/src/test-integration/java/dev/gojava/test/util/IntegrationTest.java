9bf8c113c22bfd26ea955f6933f98bb85a1cf13a
Alex Rabelo Ferreira <arf92@live.com>
2022-06-15 23:25:45 -0300

    update: integration test to use wiremock resource

--- backend/src/test-integration/java/dev/gojava/test/util/IntegrationTest.java
+++ backend/src/test-integration/java/dev/gojava/test/util/IntegrationTest.java
@@ -1,9 +1,13 @@
 package dev.gojava.test.util;
 
 import dev.gojava.test.util.container.SharedContainerResource;
+import dev.gojava.test.util.server.WiremockResource;
 import io.quarkus.test.common.QuarkusTestResource;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 @QuarkusTestResource(value = SharedContainerResource.class)
-public abstract class IntegrationTest {}
+@QuarkusTestResource(value = WiremockResource.class)
+public abstract class IntegrationTest {
+
+}
