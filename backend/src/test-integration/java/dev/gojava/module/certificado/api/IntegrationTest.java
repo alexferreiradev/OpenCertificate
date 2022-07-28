311cfd1acdf19986a2750dc0423f8df5c96cd776
Alex-Ferreira <arf92@live.com>
2022-07-27 21:37:53 -0300

    fp integration test

--- backend/src/test-integration/java/dev/gojava/module/certificado/api/IntegrationTest.java
+++ backend/src/test-integration/java/dev/gojava/module/certificado/api/IntegrationTest.java
@@ -1,13 +1,11 @@
 package dev.gojava.module.certificado.api;
 
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
+import dev.gojava.module.certificado.api.test.commons.container.CustomPostgresContainer;
 import org.testcontainers.junit.jupiter.Container;
 
 public abstract class IntegrationTest {
 
     @Container
-    protected final static GenericContainer POSTGRES_CONTAINER = new PostgreSQLContainer();
-
+    protected final static CustomPostgresContainer POSTGRES_CONTAINER = new CustomPostgresContainer();
 
 }
