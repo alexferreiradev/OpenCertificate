aecd4944d40f4575f4c2956cc126bb848e3eefbd
alexferreiradev <arf92@live.com>
2022-08-19 14:42:38 -0300

    fp migration

--- backend/src/main/resources/db/migration/V1__Create_certificate_table.sql
+++ backend/src/main/resources/db/migration/V1__Create_certificate_table.sql
@@ -1,9 +1,24 @@
-create table certificate (
-    ID int not null primary key
-    fileName varchar(255) not null,
-    fileExtension varchar(255) not null,
-    uuid varchar(255) not null
-    participantId int not null
+-- create table event ();
+
+create table participant
+(
+    id             int not null primary key,
+    name             varchar(255) not null,
+    lastName             varchar(255) not null,
+    rg           varchar(255) not null,
+    cpf          varchar(255) not null,
+    hour             varchar(255) not null,
+    event_id             int not null
 );
 
-alter table certificate add foreign key (participant_id) references (id);
+create table certificate
+(
+    id             int          not null primary key,
+    file_name      varchar(255) not null,
+    file_extension varchar(255) not null,
+    uuid           varchar(255) not null,
+    participant_id int          not null
+);
+
+alter table certificate
+    add foreign key (participant_id) references participant(id);
