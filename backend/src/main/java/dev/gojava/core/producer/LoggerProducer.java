8411a526cc29ad2dc448f3cf0d515459e1365c05
Alex Rabelo Ferreira <arf92@live.com>
2022-08-20 20:55:08 -0300

    fix: log to be dependent
    - need to setup the correct class to each injection

--- backend/src/main/java/dev/gojava/core/producer/LoggerProducer.java
+++ backend/src/main/java/dev/gojava/core/producer/LoggerProducer.java
@@ -3,13 +3,17 @@ package dev.gojava.core.producer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Scope;
+import javax.inject.Singleton;
 
 
 public class LoggerProducer {
 
     @Produces
+    @Dependent
     public Logger getLoggerSLF4J(InjectionPoint bean) {
         return LoggerFactory.getLogger(bean.getClass());
     }
