74e30cb142ed63356cb485f2ba23b202fc67e7ea
alexferreiradev <arf92@live.com>
2022-08-24 15:23:26 -0300

    update: certificate rest
    - change to integration test

--- backend/src/test-integration/java/dev/gojava/module/certificado/api/CertificadoRestIT.java
+++ backend/src/test-integration/java/dev/gojava/module/certificado/api/CertificadoRestIT.java
@@ -6,6 +6,7 @@ import dev.gojava.module.certificado.service.CertificadoService;
 import dev.gojava.module.certificado.service.generator.GeneratorType;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
+import io.restassured.RestAssured;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,31 +20,21 @@ import java.net.URISyntaxException;
 import static org.hamcrest.Matchers.equalTo;
 
 @QuarkusTest
-public class CertificadoRestIT {
-
-    @InjectMock
-    CertificadoService service;
-
-    CertificadoGeradoDTO dto;
-
-    @Inject
-    CertificadoRest rest;
-
-    @BeforeEach
-    void setUp() {
-        dto = new CertificadoGeradoDTO();
-        dto.arquivoZIP = new File("./");
-    }
+public class CertificadoRestIT extends IntegrationTest {
 
     @Test
     public void returnOk() throws URISyntaxException {
-        Mockito.when(service.criarListaCertificado(Mockito.any())).thenReturn(dto);
-
         GenerateCertForm form = new GenerateCertForm();
         form.entityName = GeneratorType.GOJAVA.name();
         form.csvFile = new File(getClass().getResource("/certificado-rest/csvOk.csv").toURI());
-        Response response = rest.salvarListaCertificado(form);
 
-        MatcherAssert.assertThat(response.getStatus(), equalTo(200));
+        // @formatter:off
+        RestAssured.given()
+                .when()
+                .multiPart("entityName", form.entityName)
+                .multiPart("csv", form.csvFile)
+                .post("/certificados")
+                .then()
+                .statusCode(Response.Status.CREATED.getStatusCode());
     }
 }
