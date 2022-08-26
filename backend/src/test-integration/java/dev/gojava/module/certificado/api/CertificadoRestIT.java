2c66d470e609ad223b4e568f5fc12580cd8cef32
Alex Rabelo Ferreira <arf92@live.com>
2022-08-25 23:38:11 -0300

    fp rest test

--- backend/src/test-integration/java/dev/gojava/module/certificado/api/CertificadoRestIT.java
+++ backend/src/test-integration/java/dev/gojava/module/certificado/api/CertificadoRestIT.java
@@ -1,29 +1,20 @@
 package dev.gojava.module.certificado.api;
 
-import dev.gojava.module.certificado.dto.CertificadoGeradoDTO;
 import dev.gojava.module.certificado.dto.GenerateCertForm;
-import dev.gojava.module.certificado.service.CertificadoService;
 import dev.gojava.module.certificado.service.generator.GeneratorType;
 import io.quarkus.test.junit.QuarkusTest;
-import io.quarkus.test.junit.mockito.InjectMock;
 import io.restassured.RestAssured;
-import org.hamcrest.MatcherAssert;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
-import javax.inject.Inject;
 import javax.ws.rs.core.Response;
 import java.io.File;
 import java.net.URISyntaxException;
-
-import static org.hamcrest.Matchers.equalTo;
 
 @QuarkusTest
 public class CertificadoRestIT extends IntegrationTest {
 
     @Test
-    public void returnOk() throws URISyntaxException {
+    public void shouldReturn200_whenInputIsValid() throws URISyntaxException {
         GenerateCertForm form = new GenerateCertForm();
         form.entityName = GeneratorType.GOJAVA.name();
         form.csvFile = new File(getClass().getResource("/certificado-rest/csvOk.csv").toURI());
@@ -36,5 +27,17 @@ public class CertificadoRestIT extends IntegrationTest {
                 .post("/certificados")
                 .then()
                 .statusCode(Response.Status.CREATED.getStatusCode());
+    }
+
+    @Test
+    public void shouldReturn400_whenInputIsinvalid() {
+        // @formatter:off
+        RestAssured.given()
+                .when()
+                .multiPart("entityName", "entity that dont exists")
+                .multiPart("csv", "fake csv file")
+                .post("/certificados")
+                .then()
+                .statusCode(Response.Status.BAD_REQUEST.getStatusCode());
     }
 }
