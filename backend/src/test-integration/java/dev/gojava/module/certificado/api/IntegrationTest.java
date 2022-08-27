cdf0aaf98f0bf658fbb005b243923edb2ca7824f
Alex Rabelo Ferreira <arf92@live.com>
2022-08-27 15:37:56 -0300

    fp integration test base

--- backend/src/test-integration/java/dev/gojava/module/certificado/api/IntegrationTest.java
+++ backend/src/test-integration/java/dev/gojava/module/certificado/api/IntegrationTest.java
@@ -1,11 +1,8 @@
 package dev.gojava.module.certificado.api;
 
-import dev.gojava.module.certificado.api.test.commons.container.CustomPostgresContainer;
-import org.testcontainers.junit.jupiter.Container;
+import io.quarkus.test.common.QuarkusTestResource;
 
+@QuarkusTestResource(SharedContainerResource.class)
 public abstract class IntegrationTest {
-
-    @Container
-    protected final static CustomPostgresContainer POSTGRES_CONTAINER = new CustomPostgresContainer();
 
 }
---
+++ backend/src/test-integration/java/dev/gojava/module/certificado/api/SharedContainerResource.java
@@ -0,0 +1,23 @@
+package dev.gojava.module.certificado.api;
+
+import dev.gojava.module.certificado.api.test.commons.container.CustomPostgresContainer;
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+import org.testcontainers.junit.jupiter.Container;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+public class SharedContainerResource implements QuarkusTestResourceLifecycleManager {
+    @Container
+    protected final static CustomPostgresContainer POSTGRES_CONTAINER = new CustomPostgresContainer();
+
+    @Override
+    public Map<String, String> start() {
+        return POSTGRES_CONTAINER.configureProperties();
+    }
+
+    @Override
+    public void stop() {
+        POSTGRES_CONTAINER.stop();
+    }
+}
