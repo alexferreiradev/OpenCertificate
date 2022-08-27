98b1bf516a9439aa22bf8dda8ad795fa59a56e81
Alex Rabelo Ferreira <arf92@live.com>
2022-08-27 15:38:14 -0300

    fp repo

--- backend/src/test-integration/java/dev/gojava/module/certificado/repository/CertificadoRepositoryImplIT.java
+++ backend/src/test-integration/java/dev/gojava/module/certificado/repository/CertificadoRepositoryImplIT.java
@@ -2,11 +2,17 @@ package dev.gojava.module.certificado.repository;
 
 import dev.gojava.module.certificado.api.IntegrationTest;
 import dev.gojava.module.certificado.model.Certificate;
+import dev.gojava.module.certificado.model.Event;
+import dev.gojava.module.certificado.model.Participant;
 import io.quarkus.test.junit.QuarkusTest;
+import org.apache.james.mime4j.dom.datetime.DateTime;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+
+import java.time.Instant;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,6 +26,24 @@ class CertificadoRepositoryImplIT extends IntegrationTest {
     @Test
     void shouldSaveCertificateWhenCallSave() {
         Certificate entity = new Certificate();
+        Participant participant = new Participant();
+        Event event = new Event();
+        event.setName("teste");
+        event.setExecutor("teste");
+        event.setTalkerTopics("teste");
+        event.setDateStarted(new Date());
+        event.setDateEnded(new Date());
+
+        participant.setEvent(event);
+        participant.setName("teste");
+        participant.setCpf("teste");
+        participant.setLastName("teste");
+        participant.setRg("teste");
+        participant.setHour("teste");
+
+        entity.setParticipant(participant);
+        entity.setFileName("teste");
+        entity.setUuid("teste");
         repository.persist(entity);
 
         Certificate firstResult = repository.findById(entity.getId());
