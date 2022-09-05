326511e7c1b1efeb1a7d3e92f4dab33c5f973cf7
alexferreiradev <arf92@live.com>
2022-09-05 14:43:48 -0300

    fp migration

--- backend/src/main/resources/db/migration/V1__Create_certificate_table.sql
+++ backend/src/main/resources/db/migration/V1__Create_certificate_table.sql
@@ -1,28 +1,29 @@
 create sequence hibernate_sequence start 1;
 
-create table event (
-    id             SERIAL not null primary key,
-    name             varchar(255) not null,
-    executor             varchar(255) not null,
-    talker_topics           varchar(255) not null,
-    date_started          timestamp not null,
-    date_ended             timestamp not null
+create table event
+(
+    id            SERIAL       not null primary key,
+    name          varchar(255) not null,
+    executor      varchar(255) not null,
+    talker_topics varchar(255) not null,
+    date_started  timestamp    not null,
+    date_ended    timestamp    not null
 );
 
 create table participant
 (
-    id             SERIAL not null primary key,
-    name             varchar(255) not null,
-    last_name             varchar(255) not null,
-    rg           varchar(255),
-    cpf          varchar(255),
-    hour             varchar(255) not null,
-    event_id             int not null
+    id        SERIAL       not null primary key,
+    name      varchar(255) not null,
+    last_name varchar(255) not null,
+    rg        varchar(255),
+    cpf       varchar(255),
+    hour      varchar(255) not null,
+    event_id  int          not null
 );
 
 create table certificate
 (
-    id             SERIAL          not null primary key,
+    id             SERIAL       not null primary key,
     file_name      varchar(255) not null,
     file_extension varchar(255) not null,
     uuid           varchar(255) not null,
@@ -30,4 +31,4 @@ create table certificate
 );
 
 alter table certificate
-    add foreign key (participant_id) references participant(id);
+    add foreign key (participant_id) references participant (id);
